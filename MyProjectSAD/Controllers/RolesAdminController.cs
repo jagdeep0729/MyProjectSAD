048f3ec5d1da80695c2235e47604c475984eda04
jagdeep0729 <jagdeep03032003@gmail.com>
2025-05-30 10:49:38 +0800

    InitialCreate

---
+++ MyProjectSAD/Controllers/RolesAdminController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using SalesInformationSystem.Models;
+using SalesInformationSystem.Data;
+using Microsoft.AspNetCore.Authorization;
+
+namespace SalesInformationSystem.Controllers
+{
+
+    [Authorize(Roles ="SuperAdmin")]
+   
+    public class RolesAdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RolesAdminController(ApplicationDbContext context,
+        UserManager<IdentityUser> userManager, RoleManager<IdentityRole>
+        roleManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var roles = _context.Roles.OrderBy(r => r.Name).ToList();
+            //Select Names from Roles Order by Names;
+            return View(roles);
+        }
+
+        public IActionResult ListUsers()
+        {
+            var users = _context.Users.OrderBy(u => u.UserName).ToList();
+            //Select UserName from Users OrderBy UserName
+
+            return View(users);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(IFormCollection collection)
+        {
+            IdentityResult ir = new IdentityResult();
+            var _role = new IdentityRole();
+            _role.Name = collection["RoleName"];
+            ir = await _roleManager.CreateAsync(_role);
+            if (!ir.Succeeded)
+            {
+                foreach (IdentityError er in ir.Errors)
+                {
+                    ModelState.AddModelError(string.Empty,
+                    er.Description);
+                }
+                ViewBag.UserMessage = "Error Addding Role";
+                return View();
+            }
+            else
+            {
+                ViewBag.UserMessage = "Role Added";
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ManageUserRoles(IFormCollection form)
+        {
+            UserRoleViewModel vm = new UserRoleViewModel();
+            var username = form["UserName"];
+            var user = _context.Users.Where(u => u.UserName == username.ToString()).FirstOrDefault();
+            if (user != null)
+            {
+                var roles = _context.Roles.ToList();
+                vm.UserName = username;
+                vm.UserId = user.Id;
+                foreach (var item in roles)
+                {
+                    RoleAssignment roleAssigned = new RoleAssignment();
+                    roleAssigned.Name = item.Name;
+                    roleAssigned.Id = item.Id;
+                    if (await _userManager.IsInRoleAsync(user, item.Name))
+                    {
+                        roleAssigned.IsChecked = true;
+                    }
+                    else
+                    {
+                        roleAssigned.IsChecked = false;
+                    }
+                    vm.UserRoles.Add(roleAssigned);
+                }
+            }
+            else
+            {
+                vm = null;
+            }
+            return View(vm);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateRoles(UserRoleViewModel updateRoles, IFormCollection form)
+        {
+            var user = _context.Users.Where(u => u.UserName ==
+            form["UserName"].ToString()).FirstOrDefault();
+            foreach (var item in updateRoles.UserRoles)
+            {
+                if (item.IsChecked)
+                {
+                    if (!await _userManager.IsInRoleAsync(user, item.Name))
+                    {
+                        await _userManager.AddToRoleAsync(user, item.Name);
+                    }
+                }
+                else if (await _userManager.IsInRoleAsync(user, item.Name))
+                {
+                    await _userManager.RemoveFromRoleAsync(user,
+                    item.Name);
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+
+
+
+    }
+
+}
---
+++ MyProjectSAD/Models/AppModel.cs
@@ -0,0 +1,20 @@
+﻿namespace MyProjectSAD.Models
+{
+    public class RoleAssignment
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool IsChecked { get; set; }
+    }
+
+    public class UserRoleViewModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public List<RoleAssignment> UserRoles { get; set; }
+        public UserRoleViewModel()
+        {
+            this.UserRoles = new List<RoleAssignment>();
+        }
+    }
+}
